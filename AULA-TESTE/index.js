437ef965414f9ecd70b67ba985101b056bd97c19
Taique.mr <taikeramos_1998@hotmail.com>
2025-06-24 16:47:47 -0300

    Atualiza index.js para exibir o nome

--- AULA-TESTE/index.js
+++ AULA-TESTE/index.js
@@ -1,3 +1,32 @@
 let nome = "João";
 
-console.log(nome);
+let xp = 4100;
+
+let level = []
+
+if (xp <= 1000) {
+    level.push("Ferro");
+} 
+else if (xp >= 1001 && xp <= 2000) {
+    level.push("Bronze");
+} 
+else if (xp >= 2001 && xp <= 5000) {
+    level.push("Prata");
+} 
+else if (xp >= 5001 && xp <= 7000) {
+    level.push("Ouro");
+}
+else if (xp >= 7001 && xp <= 8000) {
+    level.push("Platina");
+} 
+else if (xp >= 8001 && xp <= 10000) {
+    level.push("Ascendente");
+}
+else if (xp >= 9001 && xp <= 10000) {
+    level.push("Mestre");
+}
+else if (xp >= 10001) {
+    level.push("Radiante");
+}    
+
+console.log("O Heroi de nome " + nome + " esta no nivel " + level + " com " + xp + " de XP.");
